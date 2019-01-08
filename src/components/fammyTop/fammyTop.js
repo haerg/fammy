3a7de2d289a2632312624256b773e9c39235f210
Egor Afanasenko <dev.afanasenko@gmail.com>
2019-01-08 23:47:30 +0300

    simple progress

--- src/components/fammyTop/fammyTop.js
+++ src/components/fammyTop/fammyTop.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './fammyTop.css';
 import playBtn from './images/play-button.svg';
+import Progress from '@material-ui/core/LinearProgress';
 
 const playlist = [
  {
@@ -10,6 +11,7 @@ const playlist = [
   logoUrl: 'https://images.shazam.com/coverart/t441990739-b1437575238_s400.jpg',
   audioUrl: 'https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview128/v4/8f/d8/bf/8fd8bfa5-4a1d-462f-059e-c7b57b5409cd/mzaf_4287859394734930905.plus.aac.p.m4a',
   plays: 3,
+  progress: 60,
  },
  {
   id: 2,
@@ -18,6 +20,7 @@ const playlist = [
   logoUrl: 'https://images.shazam.com/coverart/t442173657-b1445949267_s400.jpg',
   audioUrl: '',
   plays: 22,
+  progress: 30,
  },
  {
   id: 3,
@@ -26,6 +29,7 @@ const playlist = [
   logoUrl: 'https://images.shazam.com/coverart/t434202110-b1421658123_s400.jpg',
   audioUrl: '',
   plays: 13,
+  progress: 90,
  },
  {
   id: 4,
@@ -34,6 +38,7 @@ const playlist = [
   logoUrl: 'https://images.shazam.com/coverart/t428550515-b1435118880_s400.jpg',
   audioUrl: '',
   plays: 13,
+  progress: 80,
  },
  {
   id: 5,
@@ -42,38 +47,7 @@ const playlist = [
   logoUrl: 'https://images.shazam.com/coverart/t430774707-b1409149756_s400.jpg',
   audioUrl: '',
   plays: 13,
- },
- {
-  id: 6,
-  track: 'Happier',
-  artist: 'Marshmello & Bastille',
-  logoUrl: 'https://images.shazam.com/coverart/t435160184-b1424704480_s400.jpg',
-  audioUrl: '',
-  plays: 13,
- },
- {
-  id: 7,
-  track: 'Backin\' It Up',
-  artist: 'Pardison Fontaine Feat. Cardi B',
-  logoUrl: 'https://images.shazam.com/coverart/t439743280-b1436481322_s400.jpg',
-  audioUrl: '',
-  plays: 13,
- },
- {
-  id: 8,
-  track: 'ZEZE',
-  artist: 'Kodak Black Feat. Offset & Travis Scott',
-  logoUrl: 'https://images.shazam.com/coverart/t443321905-b1438652073_s400.jpg',
-  audioUrl: '',
-  plays: 13,
- },
- {
-  id: 9,
-  track: 'Wake Up In The Sky',
-  artist: '',
-  logoUrl: 'https://images.shazam.com/coverart/t439575216-b1436089960_s400.jpg',
-  audioUrl: 'Gucci Mane, Bruno Mars & Kodak Black',
-  plays: 13,
+  progress: 60,
  },
 ];
 
@@ -92,7 +66,10 @@ class FammyTop extends Component {
      <div className="fammy-top__chart__track__title"><a href="">{item.track}</a></div>
      <div className="fammy-top__chart__track__artist"><a href="">{item.artist}</a></div>
     </div>
-    {/*{<div className="fammy-top__chart__track__plays">{item.plays}</div>}*/}
+    {/*<div className="fammy-top__chart__track__plays">{item.plays}</div>*/}
+     <div className="fammy-top__chart__track__progress_horizontal_view">
+      <Progress variant="determinate" value={item.progress} />
+     </div>
    </div>;
   });
   return (
