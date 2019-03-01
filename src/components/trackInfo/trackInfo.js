98406ea9fff3079916f8c643d87ebb6dbc303172
shazam <vasiliy.posudevskiy@gmail.com>
2019-03-02 00:14:36 +0300

    asdfa

---
+++ src/components/trackInfo/trackInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './trackInfo.css';
+
+class TrackInfo extends Component {
+
+ render = () => {
+  return (
+      <div>
+      TrackInfo
+      </div>
+  );
+ };
+}
+
+export default TrackInfo;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, BrowserRouter, Route, Switch } from 'react-router-dom';
+import TrackInfo from './components/trackInfo/trackInfo';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+     <Router>
+      <Switch>
+       <Route path="/tracks/:id" component={TrackInfo}/>
+       <Route path="/" component={App}/>
+      </Switch>
+     </Router>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
